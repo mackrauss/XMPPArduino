37e50cb02ff53f584e42645aa825fc2a3170ac7f
Alessandro Gnoli <tebemis@gmail.com>
2011-11-15 12:39:23 -0600

    Started the re-engineering of the library

--- XMPPClient.cpp
+++ XMPPClient.cpp
@@ -1,3 +1,11 @@
+#include <string.h>
+#include <stdarg.h>
+#include <Base64.h>
+#include <EthernetDHCP.h>
+#include <EthernetDNS.h>
+
+#include <stdio.h>  // REMOVE THIS!!!
+
 #include "XMPPClient.h"
 
 /****************/
@@ -50,219 +58,229 @@ static const prog_char PROGMEM close_template[] = "<presence type='unavailable'/
 /********************/
 /* TRANSITION TABLE */
 /********************/
-struct XMPPTransitionTableEntry {
-  XMPPState currentState;
-  XMPPState nextState;
-  char *keyword;
-};
+// struct XMPPTransitionTableEntry {
+//   XMPPState currentState;
+//   XMPPState nextState;
+//   char *keyword;
+// };
+// 
+// int connTableSize = 6;
+// XMPPTransitionTableEntry connTable[] = {{INIT, AUTH, "PLAIN"},
+//                                         {AUTH, AUTH_STREAM, "success"},
+//                                         {AUTH_STREAM, BIND, "bind"},
+//                                         {BIND, SESS, "jid"},
+//                                         {SESS, READY, "session"},
+//                                         {READY, WAIT, ""},
+//                                         {WAIT, WAIT, ""}};
 
-int connTableSize = 6;
-XMPPTransitionTableEntry connTable[] = {{INIT, AUTH, "PLAIN"},
-                                        {AUTH, AUTH_STREAM, "success"},
-                                        {AUTH_STREAM, BIND, "bind"},
-                                        {BIND, SESS, "jid"},
-                                        {SESS, READY, "session"},
-                                        {READY, WAIT, ""},
-                                        {WAIT, WAIT, ""}};
 
-XMPPClient::XMPPClient() : client(0) {
-    ;
+
+XMPPClientClass::XMPPClientClass() {
 }
 
-XMPPClient::XMPPClient(uint8_t *ip, uint16_t port) : client(ip, port) {
-    ;
+// return values:
+// 1 on success
+// 0 on error
+int XMPPClientClass::begin(uint8_t* macAddr) {
+	printf("%s", "Starting up DHCP");
+	return EthernetDHCP.begin(macAddr);
 }
 
-int XMPPClient::connect(char *username, char *server, char *resource, char *password) {
-    boolean connected = false, error = false;
-    this->username = username;
-    this->server = server;
-    this->resource = resource;
-    this->password = password;
 
-    while(!client.connect()) {
-	/* Retry connection every 1s and block until we're successful */
-	delay(1000);
-    }
+// int XMPPClient::connect(char *username, char *server, char *resource, char *password) {
+//     boolean connected = false, error = false;
+//     this->username = username;
+//     this->server = server;
+//     this->resource = resource;
+//     this->password = password;
+// 
+//     while(!client.connect()) {
+// 	/* Retry connection every 1s and block until we're successful */
+// 	delay(1000);
+//     }
+// 
+//     while(!connected && !error) {
+// 	/* Connect dat shit^H^H^H^Hcuss */
+// 	if(!client.connected()) {
+// 	    error = true;
+// 	    continue;
+// 	}
+// 
+// 	int ret;
+// 	ret = stateAction();
+// 
+// 	if(ret == -1) {
+// 	    error = true;
+// 	}
+// 
+// 	if(ret == 1) {
+// 	    connected = true;
+// 	}
+// 
+// 	if(ret) {
+// 	    continue;
+// 	}
+// 
+// 	processInput();
+//     }
+// 
+//     if(error || !connected) {
+// 	return 0;
+//     } else {
+// 	return 1;
+//     }
+// }
+// 
+// int XMPPClient::connect(char *jid, char *password) {
+// 	printf(jid);
+// 	//cout << password;
+//     /* Split the JID */
+//     /* Call connect(char*,char*,char*,char*) */
+// }
+// 
+// int XMPPClient::sendMessage(char *recipientJid, char *message) {
+//   sendTemplate(message_template, strlen(recipientJid) + strlen(message), recipientJid, message);
+// }
+// 
+// int XMPPClient::sendPresence() {
+//   sendTemplate(presence_template, 0);
+// }
+// 
+// int XMPPClient::close() {
+//   sendTemplate(close_template, 0);
+// }
+// 
+// int XMPPClient::openStream(char *server) {
+//     sendTemplate(open_stream_template, strlen(server), server);
+// }
+// 
+// int XMPPClient::authenticate(char *username, char *password) {
+//   int plainStringLen = strlen(username) + strlen(password) + 2;
+//   int encStringLen = base64_enc_len(plainStringLen);
+//   char plainString[plainStringLen];
+//   char encString[encStringLen];
+//   
+//   /* Set up our plain auth string. It's in the form:
+//    * "\0username\0password"
+//    * where \0 is the null character
+//    */
+//   memset(plainString, '\0', plainStringLen);
+//   memcpy(plainString + 1, username, strlen(username));
+//   memcpy(plainString + 2 + strlen(username), password, strlen(password));
+//   
+//   /* Encode to base64 */
+//   base64_encode(encString, plainString, plainStringLen);
+//   sendTemplate(plain_auth_template, encStringLen, encString);
+// }
+// 
+// int XMPPClient::bindResource(char *resource) {
+//   sendTemplate(bind_template, strlen(resource), resource);
+// }
+// 
+// int XMPPClient::openSession(char *server) {
+//   sendTemplate(session_request_template, strlen(server), server);
+// }
+// 
+// int XMPPClient::sendTemplate(const prog_char *temp_P, int fillLen, ...) {
+//   int tempLen = strlen_P(temp_P);
+//   char temp[tempLen];
+//   char buffer[tempLen + fillLen];
+//   va_list args;
+// 
+//   strcpy_P(temp, temp_P);
+// 
+//   va_start(args, fillLen);
+//   vsprintf(buffer, temp, args);
+//   client.write(buffer);
+// 
+//   return 1;
+// }
+// 
+// int XMPPClient::stateAction() {
+//  /*
+//  Serial.print("State = ");
+//  Serial.println(state);
+//  */
+//  switch(state) {
+//   case INIT:
+//     openStream(server);
+//     break;
+//   case AUTH:
+//     authenticate(username, password);
+//     break;
+//   case AUTH_STREAM:
+//     openStream(server);
+//     break;
+//   case BIND:
+//     bindResource(resource);
+//     break;
+//   case SESS:
+//     openSession(server);
+//     break;
+//   case READY:
+//     return 1;
+//     break;
+//   case WAIT:
+//     break;
+//   default:
+//     return -1;
+//     break;
+//  }
+//  return 0;
+// }
+// 
+// void XMPPClient::processInput() {
+//   int bufLen = 8;
+//   char buffer[bufLen];
+//   int i = 0;
+//   memset(buffer, '\0', bufLen);
+//   boolean stateChanged = false;
+// 
+//   if(!client.connected()) {
+//     state = WAIT;
+//     return;
+//   }
+// 
+//   // TODO: This process is pretty inefficient and naively implemented
+//   // It might be an idea to rewrite it cleverer
+//   while(!stateChanged) {
+//     if(client.available()) {
+//       /* Push a character from the ethernet interface into the buffer */
+//       for(i = 0 ; i < bufLen; i++) {
+//         buffer[i] = buffer[i+1];
+//       }
+//       buffer[i] = client.read();
+//       
+//       
+//       /* Ignore what we've read if it's an empty string */
+//       if(!strlen(buffer)) {
+//         continue;
+//       } else {
+//          //Serial.println(buffer);
+//       }
+//       
+//       for(int i = 0; i < connTableSize; i++) {
+//         if(state == connTable[i].currentState && strstr(buffer,connTable[i].keyword)) {
+//           
+// 	  /*
+//           Serial.println(buffer);
+//           Serial.println(connTable[i].keyword);
+//           Serial.println((int)strstr(buffer, connTable[i].keyword)); 
+//           
+//           Serial.print(connTable[i].keyword);
+//           Serial.println(" seen, transitioning");
+// 	  */
+//           
+//           state = connTable[i].nextState;
+//           client.flush();
+//           stateChanged = true;
+//           break;
+//         }
+//       }
+//     } else {
+//       delay(10);
+//     }
+//   }
+// }
 
-    while(!connected && !error) {
-	/* Connect dat shit^H^H^H^Hcuss */
-	if(!client.connected()) {
-	    error = true;
-	    continue;
-	}
-
-	int ret;
-	ret = stateAction();
-
-	if(ret == -1) {
-	    error = true;
-	}
-
-	if(ret == 1) {
-	    connected = true;
-	}
-
-	if(ret) {
-	    continue;
-	}
-
-	processInput();
-    }
-
-    if(error || !connected) {
-	return 0;
-    } else {
-	return 1;
-    }
-}
-
-int XMPPClient::connect(char *jid, char *password) {
-    /* Split the JID */
-    /* Call connect(char*,char*,char*,char*) */
-}
-
-int XMPPClient::openStream(char *server) {
-    sendTemplate(open_stream_template, strlen(server), server);
-}
-
-int XMPPClient::authenticate(char *username, char *password) {
-  int plainStringLen = strlen(username) + strlen(password) + 2;
-  int encStringLen = base64_enc_len(plainStringLen);
-  char plainString[plainStringLen];
-  char encString[encStringLen];
-  
-  /* Set up our plain auth string. It's in the form:
-   * "\0username\0password"
-   * where \0 is the null character
-   */
-  memset(plainString, '\0', plainStringLen);
-  memcpy(plainString + 1, username, strlen(username));
-  memcpy(plainString + 2 + strlen(username), password, strlen(password));
-  
-  /* Encode to base64 */
-  base64_encode(encString, plainString, plainStringLen);
-  sendTemplate(plain_auth_template, encStringLen, encString);
-}
-
-int XMPPClient::bindResource(char *resource) {
-  sendTemplate(bind_template, strlen(resource), resource);
-}
-
-int XMPPClient::openSession(char *server) {
-  sendTemplate(session_request_template, strlen(server), server);
-}
-
-int XMPPClient::sendMessage(char *recipientJid, char *message) {
-  sendTemplate(message_template, strlen(recipientJid) + strlen(message), recipientJid, message);
-}
-
-int XMPPClient::sendPresence() {
-  sendTemplate(presence_template, 0);
-}
-
-int XMPPClient::close() {
-  sendTemplate(close_template, 0);
-}
-
-int XMPPClient::sendTemplate(const prog_char *temp_P, int fillLen, ...) {
-  int tempLen = strlen_P(temp_P);
-  char temp[tempLen];
-  char buffer[tempLen + fillLen];
-  va_list args;
-
-  strcpy_P(temp, temp_P);
-
-  va_start(args, fillLen);
-  vsprintf(buffer, temp, args);
-  client.write(buffer);
-
-  return 1;
-}
-
-int XMPPClient::stateAction() {
- /*
- Serial.print("State = ");
- Serial.println(state);
- */
- switch(state) {
-  case INIT:
-    openStream(server);
-    break;
-  case AUTH:
-    authenticate(username, password);
-    break;
-  case AUTH_STREAM:
-    openStream(server);
-    break;
-  case BIND:
-    bindResource(resource);
-    break;
-  case SESS:
-    openSession(server);
-    break;
-  case READY:
-    return 1;
-    break;
-  case WAIT:
-    break;
-  default:
-    return -1;
-    break;
- }
- return 0;
-}
-
-void XMPPClient::processInput() {
-  int bufLen = 8;
-  char buffer[bufLen];
-  int i = 0;
-  memset(buffer, '\0', bufLen);
-  boolean stateChanged = false;
-
-  if(!client.connected()) {
-    state = WAIT;
-    return;
-  }
-
-  // TODO: This process is pretty inefficient and naively implemented
-  // It might be an idea to rewrite it cleverer
-  while(!stateChanged) {
-    if(client.available()) {
-      /* Push a character from the ethernet interface into the buffer */
-      for(i = 0 ; i < bufLen; i++) {
-        buffer[i] = buffer[i+1];
-      }
-      buffer[i] = client.read();
-      
-      
-      /* Ignore what we've read if it's an empty string */
-      if(!strlen(buffer)) {
-        continue;
-      } else {
-         //Serial.println(buffer);
-      }
-      
-      for(int i = 0; i < connTableSize; i++) {
-        if(state == connTable[i].currentState && strstr(buffer,connTable[i].keyword)) {
-          
-	  /*
-          Serial.println(buffer);
-          Serial.println(connTable[i].keyword);
-          Serial.println((int)strstr(buffer, connTable[i].keyword)); 
-          
-          Serial.print(connTable[i].keyword);
-          Serial.println(" seen, transitioning");
-	  */
-          
-          state = connTable[i].nextState;
-          client.flush();
-          stateChanged = true;
-          break;
-        }
-      }
-    } else {
-      delay(10);
-    }
-  }
-}
+XMPPClientClass XMPPClient;
--- XMPPClient.h
+++ XMPPClient.h
@@ -1,10 +1,7 @@
-#ifndef _H_XMPP_CLIENT
-#define _H_XMPP_CLIENT
+#ifndef __XMPP_CLIENT_H__
+#define __XMPP_CLIENT_H__
 
-#include <Base64.h>
 #include <Ethernet.h>
-#include <string.h>
-#include <stdarg.h>
 #include <avr/pgmspace.h>
 
 enum XMPPState {
@@ -17,38 +14,42 @@ enum XMPPState {
   WAIT
 };
 
-class XMPPClient {
-    private:
-	Client client;
-	char *username;
-	char *server;
-	char *password;
-	char *resource;
-	XMPPState state;
+class XMPPClientClass {
 
-	int sendTemplate(const prog_char *strTemplate, int fillLen, ...);
+private:
+	// Client client;
+	// 	char *username;
+	// 	char *server;
+	// 	char *password;
+	// 	char *resource;
+	// 	XMPPState state;
+	// 
+	// 	int sendTemplate(const prog_char *strTemplate, int fillLen, ...);
+	// 
+	// 	int openStream(char *server);
+	// 	int authenticate(char *username, char *password);
+	// 	int bindResource(char *resource);
+	// 	int openSession(char *server);
+	// 
+	// 	void processInput();
+	// 	int stateAction();
+	
+public:
+	XMPPClientClass();
+	
+	int begin(uint8_t* macAddr);
 
-	int openStream(char *server);
-	int authenticate(char *username, char *password);
-	int bindResource(char *resource);
-	int openSession(char *server);
-
-	void processInput();
-	int stateAction();
-
-
-    public:
-	XMPPClient();
-	XMPPClient(uint8_t *ip, uint16_t port);
-
-	int connect(char *username, char *server, char *resource, char *password);
-	int connect(char *jid, char *password);
-
-	int sendMessage(char *recipientJid, char *message);
-	int sendPresence();
-
-	int close();
-
+	// int connect(char *username, char *server, char *resource, char *password);
+	// 	int connect(char *jid, char *password);
+	// 
+	// 	int sendMessage(char *recipientJid, char *message);
+	// 	int sendPresence();
+	// 
+	// 	int close();
+	// 	int end();
+	
 };
 
-#endif /* _H_XMPP_CLIENT */
+extern XMPPClientClass XMPPClient;
+
+#endif // __XMPP_CLIENT_H__
