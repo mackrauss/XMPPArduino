5e722034e1b8e9540c7dfcc493969dd3d4712fd5
Alessandro Gnoli <tebemis@gmail.com>
2011-11-28 18:42:27 -0600

    Revert 39a5088c7f8774246491110991bfe827995fd9e2^..HEAD

--- XMPPClient.h
+++ XMPPClient.h
@@ -1,10 +1,14 @@
-#ifndef _H_XMPP_CLIENT
-#define _H_XMPP_CLIENT
+#ifndef __XMPP_CLIENT_H__
+#define __XMPP_CLIENT_H__
 
-#include <Base64.h>
 #include <Ethernet.h>
+<<<<<<< HEAD
+//#include "Client.h"
+#include "Base64.h"
 #include <string.h>
 #include <stdarg.h>
+=======
+>>>>>>> parent of f82e78d... Revert 3d762a6eb9fe257e092c88cc7c669ca69d2a0b84^..HEAD
 #include <avr/pgmspace.h>
 
 enum XMPPState {
@@ -17,24 +21,23 @@ enum XMPPState {
   WAIT
 };
 
-class XMPPClient {
-    private:
-	Client client;
+class XMPPClientClass {
+private:
+	Client *client;
 	char *username;
 	char *server;
 	char *password;
 	char *resource;
 	XMPPState state;
-
-	int sendTemplate(const prog_char *strTemplate, int fillLen, ...);
-
+		
+	int sendTemplate(const prog_char *strTemplate, int fillLen, ...);	
 	int openStream(char *server);
 	int authenticate(char *username, char *password);
 	int bindResource(char *resource);
-	int openSession(char *server);
-
+	int openSession(char *server);	
 	void processInput();
 	int stateAction();
+<<<<<<< HEAD
 
 
     public:
@@ -46,11 +49,24 @@ class XMPPClient {
 
 	int sendMessage(char *recipientJid, char *message);
 	int sendPresence();
-	
-	char* receiveMessage();
 
 	int close();
 
+=======
+		
+public:
+	XMPPClientClass();
+	int begin(byte* macAddr);
+	void connect(char *username, char *server, char *resource, char *password);
+	void connect(char *jid, char *password);
+	void maintain();
+	// int sendMessage(char *recipientJid, char *message);
+	// int sendPresence();
+	// int close();
+	// int end();
+>>>>>>> parent of f82e78d... Revert 3d762a6eb9fe257e092c88cc7c669ca69d2a0b84^..HEAD
 };
 
-#endif /* _H_XMPP_CLIENT */
+extern XMPPClientClass XMPPClient;
+
+#endif // __XMPP_CLIENT_H__
